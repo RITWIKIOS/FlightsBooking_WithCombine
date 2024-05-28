51b13b819fbf96cc541b6bd88f722fcc6014a93f
Ritwik Ghosh <ritwikghosh22@gmail.com>
2024-05-28 13:37:51 +0530

    initial commit

---
+++ Whoosh/WhooshApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhooshApp.swift
+//  Whoosh
+//
+//  Created by Ritwik Ghosh on 29/06/21.
+//
+
+import SwiftUI
+
+@main
+struct WhooshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
